0a748341d305ab0cb0bb9835e14c1397e3be4bbe
shempyu <shempyu@gmail.com>
2026-05-21 19:49:39 +0530

    fixed the server binding issue by changing the Flask app from a hardcoded port=5000 to using os.environ.get(PORT, 10000) with host=0.0.0.0, which Render requires

--- app.py
+++ app.py
@@ -51,8 +51,11 @@ def home():
 
 if __name__ == "__main__":
 
+    port = int(
+        os.environ.get("PORT", 10000)
+    )
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=port
     )
